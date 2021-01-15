e4939b7574ad8fd064742df8a4da98a54fe9d330
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-01-15 06:05:52 -0800

    completes and refactors index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const questions = require('./src/questions');
+const { addManager, next, addEngineer, addIntern } = require('./src/questions');
 const writeToFile = require('./src/writeToFile');
 const generateHTML = require('./src/generateHTML');
 
@@ -10,15 +10,57 @@ const config = require('./package.json');
 // prints splash screen
 console.log(logo(config).render());
 
+// declares variable to hold answers
+let manager = {};
+const engineers = [];
+const interns = [];
+
+const finish = () => {
+  // STARTS FOR TESTING PURPOSES ONLY
+  console.log('Manager:', manager);
+  console.log('Engineers:', engineers);
+  console.log('Interns:', interns);
+  // ENDS FOR TESTING PURPOSES ONLY
+
+  const htmlData = generateHTML(manager, engineers, interns);
+  !fs.existsSync('dist') && fs.mkdirSync('dist');
+  return writeToFile('./dist/index.html', htmlData);
+};
+
+const nextAction = () => {
+  return inquirer
+    .prompt(next)
+    .then(answers => {
+      switch (answers.nextAction) {
+        case 'Add an engineer':
+          return inquirer
+            .prompt(addEngineer)
+            .then(answers => {
+              engineers.push(answers);
+              nextAction();
+            })
+        case 'Add an intern':
+          return inquirer
+            .prompt(addIntern)
+            .then(answers => {
+              interns.push(answers);
+              nextAction();
+            })
+        case 'Finish building the team':
+          finish();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
 const start = () => {
   return inquirer
-    .prompt(questions)
+    .prompt(addManager)
     .then(answers => {
-      return generateHTML(answers);
-    })
-    .then(htmlData => {
-      !fs.existsSync('dist') && fs.mkdirSync('dist');
-      return writeToFile('./dist/index.html', htmlData);
+      manager = answers;
+      nextAction();
     })
     .catch(error => {
       console.log(error);
